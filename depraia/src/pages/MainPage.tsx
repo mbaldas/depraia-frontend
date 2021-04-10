e7f017df3d3f069d540d121e8c4cf618193d86d1
Matheus Wandermurem <matheusbaldas@hotmail.com>
2021-04-10 02:21:55 -0300

    mudando cor da navbar pra preto

--- depraia/src/pages/MainPage.tsx
+++ depraia/src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NavBar from "../components/NavBar/NavBar";
+import "./index.scss";
 
 const MainPage: React.FC = () => {
   return (
     <>
       <NavBar />
+      <section className="mainpage__section"></section>
     </>
   );
 };
