9ae151e2921f3030cd187483661068519185c418
Abel Cabral de Arruda <abel@ibm.com>
2021-04-25 20:37:05 -0300

    fix post

--- depraia/src/service/UserService.ts
+++ depraia/src/service/UserService.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 import User from "../model/User";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async createUser(user: User) {
-    console.log(user);
-    const response = await axios.post(`https://depraia-api.herokuapp.com/usuario`, { user })
+    axios.defaults.headers = {
+      'Content-Type': 'application/json'
+    };
+    const response = await axios.post(`https://depraia-api.herokuapp.com/cadastro`, JSON.stringify(user))
     return response.data;
   },
 };
